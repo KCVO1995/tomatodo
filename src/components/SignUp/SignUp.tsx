5466e2d823fa8ba2df0522a62e898efbc84f5ef4
KCVO1995 <943514589@qq.com>
2020-03-27 20:46:59 +0800

    基本完成 SignUp 页面，和样式

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Input} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+import {Link} from 'react-router-dom';
+import axios from '../../config/axios';
+import './SignUp.scss';
 
-export default function SignUp() {
-  return (<div>SignUp</div>)
 
-}
+const SignUp = () => {
+    const [account, setAccount] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
+    const commit = async () => {
+      console.log('commit');
+      axios.post('sign_up/user', {
+        account,
+        password,
+        password_confirmation: passwordConfirmation
+      }).then(() => {
+        console.log('注册成功');
+      }, (e) => {
+        throw new Error(e);
+      });
+    };
+    return (
+      <div className="sign-up">
+        <h1>欢迎注册 Tomatodo</h1>
+        <Input size="large" placeholder="请输入用户名" prefix={<UserOutlined/>}
+               onChange={(e) => {setAccount(e.target.value);}}/>
+        <Input.Password size="large" placeholder="请输入密码" onChange={(e) => {setPassword(e.target.value);}}/>
+        <Input.Password size="large" placeholder="请再次输入密码"
+                        onChange={(e) => {setPasswordConfirmation(e.target.value);}}/>
+        <button onClick={commit} className="login-button">注册</button>
+        如果已经有账号，请立刻<Link to="/login" className="link">登陆</Link>
+      </div>
+
+    );
+  }
+;
+
+export default SignUp;
--- src/config/axios.ts
+++ src/config/axios.ts
@@ -12,25 +12,27 @@ const instance = axios.create({
 });
 
 // 添加请求拦截器
-instance.interceptors.request.use(function (config) {
+instance.interceptors.request.use( config => {
+  console.log('请求');
   const xToken = localStorage.getItem('x-token');
   if (xToken) {config.headers.Authorization = `Bearer ${xToken}`;}
   // 在发送请求之前做些什么
   return config;
-}, function (error) {
+}, error => {
   // 对请求错误做些什么
   console.error(error);
   return Promise.reject(error);
 });
 
 // 添加响应拦截器
-instance.interceptors.response.use(function (response) {
+instance.interceptors.response.use( response => {
+  console.log('响应');
   // 对响应数据做点什么
   if (response.headers['x-token']) {
     localStorage.setItem('x-token', response.headers['x-token']);
   }
   return response;
-}, function (error) {
+}, error => {
   // 对响应错误做点什么
   return Promise.reject(error);
 });
